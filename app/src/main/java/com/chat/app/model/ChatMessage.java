3ad10bb5b83770ab4a64ed6551240c9c973707ae
Asmit Kumar Singh <asmit.kumarsingh.cse26@heritageit.edu.in>
2026-07-11 20:22:47 +0530

    Fullstack Chat App with SpringBoot & thymleaf

---
+++ app/src/main/java/com/chat/app/model/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.chat.app.model;
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+}
